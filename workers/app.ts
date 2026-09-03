a158fe80c28a91885de66bee4abeb0500b3f19b3
Joao Miguel Erig Bohn <joao.miguel.bohn@gmail.com>
2026-09-03 21:19:46 +0200

    Migrate deployment from Vercel to Cloudflare Workers

---
+++ workers/app.ts
@@ -0,0 +1,19 @@
+import { createContext, createRequestHandler, RouterContextProvider } from "react-router";
+
+export const cloudflareContext = createContext<{
+  env: Env;
+  ctx: ExecutionContext;
+}>();
+
+const requestHandler = createRequestHandler(
+  () => import("virtual:react-router/server-build"),
+  import.meta.env.MODE,
+);
+
+export default {
+  fetch(request, env, ctx) {
+    const context = new RouterContextProvider();
+    context.set(cloudflareContext, { env, ctx });
+    return requestHandler(request, context);
+  },
+} satisfies ExportedHandler<Env>;
